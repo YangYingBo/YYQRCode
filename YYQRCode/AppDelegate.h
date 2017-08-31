d2a2a880c736f171fb9f27ce354fc70978415f43
Loser <yang_ying_bo@sina.com>
2017-08-31 15:27:14 +0800

    Initial Commit

---
+++ YYQRCode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YYQRCode
+//
+//  Created by Loser on 2017/8/31.
+//  Copyright © 2017年 Loser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
